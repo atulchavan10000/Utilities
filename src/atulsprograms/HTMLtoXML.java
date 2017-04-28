b796910224fe5335bedbd0ee7104f581818efabd
atulchavan10000 <atulchavan528@gmail.com>
2017-04-28 20:22:02 +0530

    HTMLtoXML

---
+++ src/atulsprograms/HTMLtoXML.java
@@ -0,0 +1,42 @@
+package atulsprograms;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+public class HTMLtoXML {
+	public static void main(String args[]) throws IOException, ParserConfigurationException, SAXException, XPathExpressionException{
+		String field;
+		String content = HTMLtoXML.readFile("D:\\source.txt", StandardCharsets.UTF_8);
+
+		InputSource source = new InputSource(new StringReader(content));
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db = dbf.newDocumentBuilder();
+		Document document = db.parse(source);
+
+		XPathFactory xpathFactory = XPathFactory.newInstance();
+		XPath xpath = xpathFactory.newXPath();
+
+		field = xpath.evaluate("//bundle//trackingNumber", document);
+		System.out.println(field);
+	}
+	static String readFile(String path, Charset encoding) throws IOException 
+	{
+		byte[] encoded = Files.readAllBytes(Paths.get(path));
+		return new String(encoded, encoding);
+	}
+}
